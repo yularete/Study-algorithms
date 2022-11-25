b3ee4548ffa69c2873dc0119df8c021a917a65b1
yularete <keinikeiwo@gmail.com>
2022-11-25 23:26:04 +0900

    fail : 백준 / 새싹 / 문자열 / 11654 (푸는중 ㅠ___________________________________________________________________________ㅠ)

---
+++ baekjoon/src/sprout/strings/Strings11654.java
@@ -0,0 +1,12 @@
+package sprout.strings;
+
+import java.util.Scanner;
+
+public class Strings11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.nextLine();
+        int B = (int)A;
+        System.out.println(B);
+    }
+}
