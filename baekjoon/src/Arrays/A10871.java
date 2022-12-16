4710e0a3e91d857206d6453fe5a3287e2ed2c56d
yularete <keinikeiwo@gmail.com>
2022-12-16 10:00:01 +0900

    백준 /새싹 / 배열 / 10871

---
+++ baekjoon/src/Arrays/A10871.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class A10871 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int A = sc.nextInt();
+            if (A < X)
+                System.out.print(A + " ");
+        }
+    }
+}
