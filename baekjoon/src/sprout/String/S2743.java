d103830ce5032e002b06504634bf001beacd4873
yularete <keinikeiwo@gmail.com>
2023-02-17 23:25:43 +0900

    백준 /새싹 / 문자열 / 2743

---
+++ baekjoon/src/sprout/String/S2743.java
@@ -0,0 +1,12 @@
+package sprout.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine().length());
+    }
+}
