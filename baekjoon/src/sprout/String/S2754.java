3cd68bc22439f0d4a866d1c8704fb46d2ce220c6
yularete <keinikeiwo@gmail.com>
2023-02-18 22:16:41 +0900

    백준 /새싹 / 문자열 / 2754

---
+++ baekjoon/src/sprout/String/S2754.java
@@ -0,0 +1,43 @@
+package sprout.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S2754{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String score = br.readLine();
+
+        switch(score) {
+            //꼭 break를 해줘야 해당 값을 출력후 switch를 종료시킨다.
+            //없다면 그 아래 케이스를 다 출력해 버리기때문이다.
+            case "A+": System.out.println(4.3);
+                break;
+            case "A0": System.out.println(4.0);
+                break;
+            case "A-": System.out.println(3.7);
+                break;
+            case "B+": System.out.println(3.3);
+                break;
+            case "B0": System.out.println(3.0);
+                break;
+            case "B-": System.out.println(2.7);
+                break;
+            case "C+": System.out.println(2.3);
+                break;
+            case "C0": System.out.println(2.0);
+                break;
+            case "C-": System.out.println(1.7);
+                break;
+            case "D+": System.out.println(1.3);
+                break;
+            case "D0": System.out.println(1.0);
+                break;
+            case "D-": System.out.println(0.7);
+                break;
+            case "F": System.out.println(0.0);
+                break;
+        }
+    }
+    }
