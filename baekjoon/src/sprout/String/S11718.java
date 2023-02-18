0683df69686424abec6b82e4aa239732720a9d03
yularete <keinikeiwo@gmail.com>
2023-02-18 23:08:20 +0900

    백준 /새싹 / 문자열 / 11718

---
+++ baekjoon/src/sprout/String/S11718.java
@@ -0,0 +1,16 @@
+package sprout.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class S11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+    }
+}
