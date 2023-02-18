08b38f66f25c5767af9875d13ac8f831702adc51
yularete <keinikeiwo@gmail.com>
2023-02-18 20:58:10 +0900

    백준 /새싹 / 문자열 / 2744

---
+++ baekjoon/src/sprout/String/S2744.java
@@ -0,0 +1,25 @@
+package sprout.String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class S2744 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String inputData = br.readLine();
+
+        for (int i=0; i < inputData.length(); i++){
+            char c = inputData.charAt(i);
+
+            if(Character.isUpperCase(c)){
+                System.out.print(String.valueOf(c).toLowerCase());
+            } else {
+                System.out.print(String.valueOf(c).toUpperCase());
+            }
+        }
+    }
+}
