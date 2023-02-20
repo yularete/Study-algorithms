ba25944b3ecc74429cd06ebc5173603f7afa0157
yularete <keinikeiwo@gmail.com>
2023-02-20 22:32:38 +0900

    백준 / CLASS 1+ / 2675

---
+++ baekjoon/src/level1/class1plus/C2675.java
@@ -0,0 +1,29 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i =0; i<T; i++){
+
+            String[] str = br.readLine().split(" ");
+
+            int R = Integer.parseInt(str[0]);
+
+            for(byte val : str[1].getBytes()) {
+                for(int j = 0; j<R; j++){
+                    sb.append((char)val);
+                }
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
