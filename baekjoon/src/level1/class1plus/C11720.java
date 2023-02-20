e0e74783741f99b78147ecdf1a8617d1ff62387f
yularete <keinikeiwo@gmail.com>
2023-02-20 23:00:02 +0900

    백준 / CLASS 1+ / 11720

---
+++ baekjoon/src/level1/class1plus/C11720.java
@@ -0,0 +1,19 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C11720 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+
+        int sum = 0;
+
+        for(byte value : br.readLine().getBytes()){
+            sum += (value - '0');
+        }
+        System.out.print(sum);
+    }
+}
