ae5df18aa59e932235b3df8db4df4b5dd7ca1afd
yularete <keinikeiwo@gmail.com>
2023-02-21 11:23:29 +0900

    백준 / CLASS 1++ / 1546

--- baekjoon/src/level1/class1plus2/C1546.java
+++ baekjoon/src/level1/class1plus2/C1546.java
@@ -1,4 +1,28 @@
 package level1.class1plus2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class C1546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); //입력 개수
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = -1;
+        double sum = 0.0;
+
+        for (int i = 0; i < N; i++) {
+            int value = Integer.parseInt(st.nextToken());
+
+            if (value > max) {
+                max = value;
+            }
+            sum += value;
+        }
+        System.out.println(((sum / max) * 100.0) / N);
+    }
 }
