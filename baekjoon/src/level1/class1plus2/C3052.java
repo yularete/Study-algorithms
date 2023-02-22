780b9c14ae1549c6165936202f254116cffe2bd8
yularete <keinikeiwo@gmail.com>
2023-02-22 22:56:33 +0900

    백준 / CLASS 1++ / 3052

---
+++ baekjoon/src/level1/class1plus2/C3052.java
@@ -0,0 +1,18 @@
+package level1.class1plus2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class C3052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<Integer> h = new HashSet<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            h.add(Integer.parseInt(br.readLine()) % 42);
+        }
+        System.out.print(h.size());
+    }
+}
