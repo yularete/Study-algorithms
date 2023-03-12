292d6d4cdefca27f2d500d750fa599be10c2f15c
yularete <keinikeiwo@gmail.com>
2023-03-12 23:48:15 +0900

    백준 / CLASS 2 / 2869

---
+++ baekjoon/src/level2/class2/C2869.java
@@ -0,0 +1,27 @@
+package level2.class2;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class C2869 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int up = Integer.parseInt(st.nextToken());
+        int down = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(st.nextToken());
+
+        int day = (length - down) / (up - down);
+        if ((length - down) % (up - down) != 0)
+            day++;
+
+        System.out.println(day);
+    }
+}
+}
