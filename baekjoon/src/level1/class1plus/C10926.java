d84dbff154c758d3859879101081c004eda919cb
yularete <keinikeiwo@gmail.com>
2023-03-17 22:33:57 +0900

    백준 / CLASS 2 /10926

---
+++ baekjoon/src/level1/class1plus/C10926.java
@@ -0,0 +1,15 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C10926 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String lastString = "??!";
+
+        String s = br.readLine();
+        System.out.println(s + lastString);
+    }
+}
