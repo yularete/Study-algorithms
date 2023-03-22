5482281b29b6bbad813a2940407fe488bcb0e17d
yularete <keinikeiwo@gmail.com>
2023-03-22 23:18:49 +0900

    백준 / CLASS 1 / C2525

---
+++ baekjoon/src/level1/class1plus/C2525.java
@@ -0,0 +1,25 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class C2525 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int C = Integer.parseInt(br.readLine());
+
+        int min = 60 * A + B;
+        min += C;
+
+        int hour = (min / 60) % 24;
+        int minute = min % 60;
+
+        System.out.println(hour + " " +minute);
+    }
+}
