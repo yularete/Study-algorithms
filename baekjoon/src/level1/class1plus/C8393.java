064dab78dda6ecc61b92aafce2d45eb30f485e68
yularete <keinikeiwo@gmail.com>
2023-03-24 22:48:41 +0900

    백준 / CLASS 1 / C8393

---
+++ baekjoon/src/level1/class1plus/C8393.java
@@ -0,0 +1,18 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C8393 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int sum = 0;
+        for (int i = 1; i <= num; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
