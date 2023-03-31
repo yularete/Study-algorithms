aa6c546534668b16284f8957e0555f5b5f762f20
yularete <keinikeiwo@gmail.com>
2023-03-31 23:42:55 +0900

    백준 / CLASS 1 / C10811

---
+++ baekjoon/src/level1/class1plus/C10811.java
@@ -0,0 +1,44 @@
+package level1.class1plus;
+
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class C10811 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N + 1];
+        for (int i = 1; i <= N; i++)
+            arr[i] = i;
+
+
+        for (int k = 0; k < M; k++) {
+            Stack<Integer> stack = new Stack<>();
+            st = new StringTokenizer(bf.readLine(), " ");
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            for (int m = i; m <= j; m++)
+                stack.push(arr[m]);
+
+            for (int m = i; m <= j; m++)
+                arr[m] = stack.pop();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++)
+            sb.append(arr[i]).append(" ");
+
+        bw.write(sb.toString() + "\n");
+        bw.flush();
+        bw.close();
+        bf.close();
+    }
+
+}
