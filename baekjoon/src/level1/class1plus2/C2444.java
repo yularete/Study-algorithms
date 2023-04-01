836c18767618d784ab982b04e98abfa7f1db674e
yularete <keinikeiwo@gmail.com>
2023-04-01 23:40:15 +0900

    백준 / CLASS 1 / C2444

---
+++ baekjoon/src/level1/class1plus2/C2444.java
@@ -0,0 +1,34 @@
+package level1.class1plus2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C2444 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= num; i++) {
+            for (int j = 1; j <= num-i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for (int i = num-1; i >= 1; i--) {
+            for (int j = 1; j <= num-i; j++) {
+                sb.append(" ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
