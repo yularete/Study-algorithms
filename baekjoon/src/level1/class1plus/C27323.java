30fdec128d0cdefe1582741b314850e5925ec9f1
yularete <keinikeiwo@gmail.com>
2023-04-27 23:33:18 +0900

    백준 / CLASS 1 / C27323

---
+++ baekjoon/src/level1/class1plus/C27323.java
@@ -0,0 +1,12 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class C27323 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine()), B = Integer.parseInt(br.readLine());
+        System.out.println(A * B);
+    }
+}
