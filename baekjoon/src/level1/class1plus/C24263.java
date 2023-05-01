ff52063ca4ddf8595cba733ba547b086e81a6083
yularete <keinikeiwo@gmail.com>
2023-05-01 23:39:54 +0900

    백준 / CLASS 1 / C24263

---
+++ baekjoon/src/level1/class1plus/C24263.java
@@ -0,0 +1,16 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public interface C24263 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println(n);
+        System.out.println(1);
+    }
+}
