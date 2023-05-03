e087c5b73493e48ea7f5b2a3941209b01f4282cb
yularete <keinikeiwo@gmail.com>
2023-05-03 23:06:46 +0900

    백준 / CLASS 1 / C24265

---
+++ baekjoon/src/level1/class1plus/C24265.java
@@ -0,0 +1,15 @@
+package level1.class1plus;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C24265 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+
+        System.out.println((n * (n - 1)) / 2);
+        System.out.println(2);
+    }
+}
