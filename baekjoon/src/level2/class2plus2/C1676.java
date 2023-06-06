534bfab22c2ae5b6a838712c88ed3aea016aefbc
yularete <keinikeiwo@gmail.com>
2023-06-06 23:40:10 +0900

    백준 / CLASS 2 / C1676

---
+++ baekjoon/src/level2/class2plus2/C1676.java
@@ -0,0 +1,19 @@
+package level2.class2plus2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+    }
+}
