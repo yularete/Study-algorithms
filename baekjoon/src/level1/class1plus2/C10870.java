6906ea19bc6c02415f74a1fdeaa3cc1e32930bc1
yularete <keinikeiwo@gmail.com>
2023-06-16 12:52:48 +0900

    백준 / CLASS 1 / C10870

---
+++ baekjoon/src/level1/class1plus2/C10870.java
@@ -0,0 +1,20 @@
+package level1.class1plus2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C10870 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(N));
+    }
+
+    static int fibonacci(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        return fibonacci(N - 1) + fibonacci(N - 2);
+    }
+}
