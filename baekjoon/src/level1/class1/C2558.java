11e9e0546a1057a35871448687f2bbebca5c7474
yularete <keinikeiwo@gmail.com>
2023-08-17 23:10:02 +0900

    백준 / CLASS 1 / C2558

---
+++ baekjoon/src/level1/class1/C2558.java
@@ -0,0 +1,13 @@
+package level1.class1;
+
+import java.util.Scanner;
+
+public class C2558 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        in.close();
+        System.out.println(a + b);
+    }
+}
