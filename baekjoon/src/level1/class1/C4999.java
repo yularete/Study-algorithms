c9c759a0af0df1c7839b53de3482dafeebd16d05
yularete <keinikeiwo@gmail.com>
2023-08-19 23:53:54 +0900

    백준 / CLASS 1 / C4999

---
+++ baekjoon/src/level1/class1/C4999.java
@@ -0,0 +1,18 @@
+package level1.class1;
+
+import java.util.Scanner;
+
+public class C4999 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String a = in.next();
+        String b = in.next();
+
+        if (a.length() >= b.length()) {
+            System.out.println("go");
+        } else {
+            System.out.println("noßß");
+        }
+    }
+}
