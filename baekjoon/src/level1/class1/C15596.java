3be1fb5376f544ada6c236a1f7715bbe6c315487
yularete <keinikeiwo@gmail.com>
2023-08-21 22:35:33 +0900

    백준 / CLASS 1 / C15596

---
+++ baekjoon/src/level1/class1/C15596.java
@@ -0,0 +1,11 @@
+package level1.class1;
+
+public class C15596 {
+    long sum(int[] a){
+        long sum = 0;
+        for (int j : a) {
+            sum += j;
+        }
+        return sum;
+    }
+}
