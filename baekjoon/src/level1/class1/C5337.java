c4924c006ccf0f1cb97ec154529b4e8dbdaf6488
yularete <keinikeiwo@gmail.com>
2023-08-27 23:39:55 +0900

    백준 / CLASS 1 / C5337

---
+++ baekjoon/src/level1/class1/C5337.java
@@ -0,0 +1,7 @@
+package level1.class1;
+
+public class C5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" + "|  | _ | _. _ ._ _  _\n" + "|/\\|(/.|(_.(_)[ | )(/.\n");
+    }
+}
