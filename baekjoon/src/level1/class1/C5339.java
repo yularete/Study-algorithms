8da5136bd37edee8d0a1464f103b5c767fb63f08
yularete <keinikeiwo@gmail.com>
2023-08-30 23:09:27 +0900

    백준 / CLASS 1 / C5339

---
+++ baekjoon/src/level1/class1/C5339.java
@@ -0,0 +1,18 @@
+package level1.class1;
+
+import java.io.IOException;
+
+public class C5339 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
