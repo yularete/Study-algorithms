bb9e88dcf68dfa0e985f80686ab614983019bafa
yularete <keinikeiwo@gmail.com>
2023-08-31 23:34:25 +0900

    백준 / CLASS 1 / C16394

---
+++ baekjoon/src/level1/class1/C16394.java
@@ -0,0 +1,13 @@
+package level1.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C16394 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.print(n - 1946);
+    }
+}
