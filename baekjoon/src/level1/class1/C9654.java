40e637327c1d33bdbe7e81a77bc81881285b9d78
yularete <keinikeiwo@gmail.com>
2023-09-01 23:58:12 +0900

    백준 / CLASS 1 / C9654

---
+++ baekjoon/src/level1/class1/C9654.java
@@ -0,0 +1,14 @@
+package level1.class1;
+
+import java.io.IOException;
+
+public class C9654 {
+    public static void main(String[] args) throws IOException {
+        System.out.print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+                "N2 Bomber      Heavy Fighter  Limited    21        \n" +
+                "J-Type 327     Light Combat   Unlimited  1         \n" +
+                "NX Cruiser     Medium Fighter Limited    18        \n" +
+                "N1 Starfighter Medium Fighter Unlimited  25        \n" +
+                "Royal Cruiser  Light Combat   Limited    4         ");
+    }
+}
