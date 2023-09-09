81f084ece4e33bd6f2bfc7d9ae4578116a260bf4
yularete <keinikeiwo@gmail.com>
2023-09-09 22:37:59 +0900

    백준 / CLASS 1 / C3046

---
+++ baekjoon/src/level1/class1/C3046.java
@@ -0,0 +1,15 @@
+package level1.class1;
+
+import java.util.Scanner;
+
+public class C3046 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println((b * 2) - a);
+
+    }
+}
