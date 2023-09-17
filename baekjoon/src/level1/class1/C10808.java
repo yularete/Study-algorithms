e1fc480db3d8020452e68bbf8899838b526ead86
yularete <keinikeiwo@gmail.com>
2023-09-17 11:46:37 +0900

    백준 / CLASS 1 / C10808

---
+++ baekjoon/src/level1/class1/C10808.java
@@ -0,0 +1,21 @@
+package level1.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class C10808 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[26];
+        String s = br.readLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            arr[c - 97]++;
+        }
+        for (int i = 0; i < 26; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
