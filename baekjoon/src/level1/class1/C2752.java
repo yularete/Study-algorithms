b7902cb4c2705d295cf269ec83e7b4777cde0e32
yularete <keinikeiwo@gmail.com>
2023-09-18 21:16:51 +0900

    백준 / CLASS 1 / C2752

---
+++ baekjoon/src/level1/class1/C2752.java
@@ -0,0 +1,22 @@
+package level1.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class C2752 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+}
