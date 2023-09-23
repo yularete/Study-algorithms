cf8e0afa4d8d60884959b48b8f8f189a0d0e6d81
yularete <keinikeiwo@gmail.com>
2023-09-23 23:18:51 +0900

    백준 / CLASS 1 / C11719

---
+++ baekjoon/src/level1/class1/C11719.java
@@ -0,0 +1,19 @@
+package level1.class1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class C11719 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+
+    }
+}
