b106ce1fb266d7ba8269e9eee6c62bdd35dc9f6b
yularete <keinikeiwo@gmail.com>
2023-09-28 23:05:01 +0900

    백준 / CLASS 1 / C6749

---
+++ baekjoon/src/level1/class1/C6749.java
@@ -0,0 +1,14 @@
+package level1.class1;
+
+import java.util.Scanner;
+
+public class C6749 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+
+        System.out.println(m + (m - y));
+    }
+}
