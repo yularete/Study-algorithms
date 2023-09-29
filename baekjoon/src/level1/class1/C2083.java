93f0d4c9ce38f12a27454a4bdf3f564f0d0f0a5e
yularete <keinikeiwo@gmail.com>
2023-09-29 22:33:44 +0900

    백준 / CLASS 1 / C2083

---
+++ baekjoon/src/level1/class1/C2083.java
@@ -0,0 +1,25 @@
+package level1.class1;
+
+import java.util.Scanner;
+
+public class C2083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String name = sc.next();
+            int age = sc.nextInt();
+            int weight = sc.nextInt();
+
+            if (name.equals("#") && age == 0 && weight == 0) {
+                break;
+            }
+            if (age > 17 || weight >= 80) {
+                System.out.println(name + " Senior");
+            } else {
+                System.out.println(name + " Junior");
+            }
+        }
+        sc.close();
+    }
+}
